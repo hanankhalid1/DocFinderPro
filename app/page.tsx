0a454521e6baac22b5d07d90e1243ee990931309
hanankhalid1 <hanankhalid1434@gmail.com>
2024-05-05 00:13:21 +0500

    Header, Hero Section Search Bar added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CategorySearch from "./_components/CategorySearch";
+import Hero from "./_components/Hero";
+
+export default function Home() {
+  return <div>
+    {/* Hero Section */}
+    <Hero/>
+    {/* Search Bar + Category */}
+    <CategorySearch/>
+  </div>;
+}
