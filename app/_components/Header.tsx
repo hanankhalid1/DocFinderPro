5708b37d7f604774f9bab248f9a0f70e8ec32a59
hanankhalid1 <hanankhalid1434@gmail.com>
2024-05-11 19:26:25 +0500

    Added Kinde Authentication Feature

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,8 +1,18 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  LoginLink,
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const Header = () => {
   const Menu = [
@@ -19,16 +29,23 @@ const Header = () => {
     {
       id: 3,
       name: "Contact Us",
-      path: "/conatct",
+      path: "/contact", // corrected typo in path
     },
   ];
+
+  // User Information
+  const { user } = useKindeBrowserClient();
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <div className="flex items-center justify-between p-4 shadow-sm">
       <div className="flex items-center gap-10">
         <Image src="/logo.svg" alt="logo" width={180} height={80} />
-        <ul className=" md:flex gap-8 hidden">
+        <ul className="md:flex gap-8 hidden">
           {Menu.map((item, index) => (
-            <Link href={item.path}>
+            <Link key={item.id} href={item.path}>
               <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
                 {item.name}
               </li>
@@ -36,7 +53,30 @@ const Header = () => {
           ))}
         </ul>
       </div>
-      <Button>Get Started</Button>
+      {user ? (
+        <div className="flex items-center">
+          <Popover>
+            <PopoverTrigger><Image
+            src={user?.picture}
+            alt="Profile Picture"
+            height={50}
+            width={50}
+            className="rounded-full"
+          /></PopoverTrigger>
+            <PopoverContent className="w-44"><ul className="flex flex-col gap-2">
+              <li className="cursor-pointer hover: bg-slate-100 p-2 rounded-md">Profile</li>
+              <li className="cursor-pointer bg-slate-100 p-2 rounded-md">My Booking</li>
+              <li className="cursor-pointer bg-slate-100 p-2 rounded-md"><LogoutLink>Logout</LogoutLink></li>
+              </ul></PopoverContent>
+          </Popover>
+
+          
+        </div>
+      ) : (
+        <LoginLink>
+          <Button>Get Started</Button>
+        </LoginLink>
+      )}
     </div>
   );
 };
